7bac4a66df65812129cdca013a8721702480d661
KantoCanto <bruno.canto.neves@gmail.com>
2023-11-09 14:06:46 +0000

    need to fix Navbar

--- CV_Vue/src/components/Navbar.vue
+++ CV_Vue/src/components/Navbar.vue
@@ -3,18 +3,27 @@ import { RouterLink } from "vue-router";
 </script>
 
 <template>
-  <nav>
-    <div class="wrapper">
+  <nav class="navbar navbar-expand-lg">
+    <div class="wrapper" id="responsiveNav">
       <div class="logo">
         <RouterLink to="/"
           ><img src="../assets/logo.png" alt="logo"
         /></RouterLink>
       </div>
-      <div class="items">
-        <ul>
-          <RouterLink to="about"><li>About Me</li></RouterLink>
-          <RouterLink to="what-i-do"><li>What I Do</li></RouterLink>
-          <RouterLink to="hobbies"><li>My Hobbies</li></RouterLink>
+      <div class="collapse">
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <RouterLink to="/about" class="nav-link">About Me</RouterLink>
+          </li>
+          <li class="nav-item">
+            <RouterLink to="/what-i-do" class="nav-link">What I do</RouterLink>
+          </li>
+          <li class="nav-item">
+            <RouterLink to="/hobbies" class="nav-link">My Hobbies</RouterLink>
+          </li>
+          <a href="javascript:void(0);" class="icon" onclick="myFunction()">
+            <i class="fa fa-bars"></i>
+          </a>
         </ul>
       </div>
     </div>
@@ -22,6 +31,9 @@ import { RouterLink } from "vue-router";
 </template>
 
 <style scoped>
+.icon {
+  display: none;
+}
 .wrapper {
   display: flex;
   justify-content: space-between;
@@ -31,7 +43,6 @@ import { RouterLink } from "vue-router";
 ul {
   list-style: none;
   padding: 0;
-  margin: 0;
 }
 
 li {
@@ -39,12 +50,37 @@ li {
   margin: 0 1rem 0 1rem;
 }
 
-nav {
-  color: antiquewhite;
-}
-
 img {
   height: 4rem;
   width: 4rem;
+}
+
+.container-fluid {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.collapse {
+  display: flex;
+}
+.nav-link {
+  text-decoration: none;
+  cursor: pointer;
+  white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  max-width: 100%;
+  color: hsla(160, 100%, 37%, 1);
+}
+
+.nav-link :hover {
+  color: antiquewhite;
+}
+
+@media (min-width: 500px) {
+  ul {
+    margin-left: auto;
+  }
 }
 </style>
