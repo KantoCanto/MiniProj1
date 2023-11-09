3e4ee2797fce7b594f16fcee060ae088ced3d8c0
KantoCanto <bruno.canto.neves@gmail.com>
2023-11-09 15:56:35 +0000

    Navbar is finished

---
+++ CV_Vue/src/components/BrokenNavbar.vue
@@ -0,0 +1,86 @@
+<script setup>
+import { RouterLink } from "vue-router";
+</script>
+
+<template>
+  <nav class="navbar navbar-expand-lg">
+    <div class="wrapper" id="responsiveNav">
+      <div class="logo">
+        <RouterLink to="/"
+          ><img src="../assets/logo.png" alt="logo"
+        /></RouterLink>
+      </div>
+      <div class="collapse">
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <RouterLink to="/about" class="nav-link">About Me</RouterLink>
+          </li>
+          <li class="nav-item">
+            <RouterLink to="/what-i-do" class="nav-link">What I do</RouterLink>
+          </li>
+          <li class="nav-item">
+            <RouterLink to="/hobbies" class="nav-link">My Hobbies</RouterLink>
+          </li>
+          <a href="javascript:void(0);" class="icon" onclick="myFunction()">
+            <i class="fa fa-bars"></i>
+          </a>
+        </ul>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<style scoped>
+.icon {
+  display: none;
+}
+.wrapper {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+
+ul {
+  list-style: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 1rem 0 1rem;
+}
+
+img {
+  height: 4rem;
+  width: 4rem;
+}
+
+.container-fluid {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.collapse {
+  display: flex;
+}
+.nav-link {
+  text-decoration: none;
+  cursor: pointer;
+  white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  max-width: 100%;
+  color: hsla(160, 100%, 37%, 1);
+}
+
+.nav-link :hover {
+  color: antiquewhite;
+}
+
+@media (min-width: 500px) {
+  ul {
+    margin-left: auto;
+  }
+}
+</style>
--- CV_Vue/src/components/Navbar.vue
+++ CV_Vue/src/components/Navbar.vue
@@ -1,17 +1,42 @@
 <script setup>
+import { ref, onMounted } from "vue";
 import { RouterLink } from "vue-router";
+
+const navbarLinksRef = ref(null);
+
+const toggleNavbar = () => {
+  console.log("Toggle button clicked");
+  if (navbarLinksRef.value) {
+    navbarLinksRef.value.classList.toggle("active");
+  }
+};
+
+const closeNavbar = () => {
+  if (navbarLinksRef.value) {
+    navbarLinksRef.value.classList.remove("active");
+  }
+};
+
+onMounted(() => {
+  navbarLinksRef.value = document.querySelector(".navbar-links");
+});
 </script>
 
 <template>
-  <nav class="navbar navbar-expand-lg">
-    <div class="wrapper" id="responsiveNav">
+  <div>
+    <nav class="navbar">
       <div class="logo">
         <RouterLink to="/"
           ><img src="../assets/logo.png" alt="logo"
         /></RouterLink>
       </div>
-      <div class="collapse">
-        <ul class="navbar-nav">
+      <a href="#" class="toggle-button" @click="toggleNavbar">
+        <span class="bar"></span>
+        <span class="bar"></span>
+        <span class="bar"></span>
+      </a>
+      <div class="navbar-links" @click="closeNavbar">
+        <ul>
           <li class="nav-item">
             <RouterLink to="/about" class="nav-link">About Me</RouterLink>
           </li>
@@ -21,33 +46,29 @@ import { RouterLink } from "vue-router";
           <li class="nav-item">
             <RouterLink to="/hobbies" class="nav-link">My Hobbies</RouterLink>
           </li>
-          <a href="javascript:void(0);" class="icon" onclick="myFunction()">
-            <i class="fa fa-bars"></i>
-          </a>
         </ul>
       </div>
-    </div>
-  </nav>
+    </nav>
+  </div>
 </template>
 
 <style scoped>
-.icon {
-  display: none;
+* {
+  box-sizing: border-box;
 }
-.wrapper {
+.navbar {
   display: flex;
+  flex-direction: column;
   justify-content: space-between;
   align-items: center;
 }
-
-ul {
-  list-style: none;
-  padding: 0;
+nav {
+  line-height: 1.3;
+  font-family: Menlo, monospace;
 }
 
-li {
-  display: inline-block;
-  margin: 0 1rem 0 1rem;
+.logo {
+  margin: 0;
 }
 
 img {
@@ -55,32 +76,69 @@ img {
   width: 4rem;
 }
 
-.container-fluid {
+.navbar-links ul {
+  margin: 0;
+  padding: 0;
   display: flex;
-  justify-content: center;
-  align-items: center;
+}
+.navbar-links li {
+  list-style: none;
+  padding: 1rem;
+  display: block;
 }
 
-.collapse {
-  display: flex;
-}
-.nav-link {
-  text-decoration: none;
-  cursor: pointer;
-  white-space: nowrap;
-  overflow: hidden;
-  text-overflow: ellipsis;
-  max-width: 100%;
+.navbar-links li :hover {
   color: hsla(160, 100%, 37%, 1);
 }
-
-.nav-link :hover {
+.nav-link {
   color: antiquewhite;
 }
 
-@media (min-width: 500px) {
-  ul {
-    margin-left: auto;
+.toggle-button {
+  position: absolute;
+  top: 1.5rem;
+  right: 1rem;
+  display: none;
+  flex-direction: column;
+  justify-content: space-between;
+  width: 2rem;
+  height: 1.75rem;
+}
+
+.toggle-button .bar {
+  height: 0.3rem;
+  width: 100%;
+  background-color: antiquewhite;
+  border-radius: 1rem;
+}
+
+@media (max-width: 500px) {
+  .toggle-button {
+    display: flex;
+  }
+  .navbar-links {
+    display: none;
+  }
+  .navbar-links.active {
+    display: flex;
+  }
+  .navbar {
+    flex-direction: row;
+  }
+
+  .navbar-links ul {
+    display: flex;
+    flex-direction: column;
+    position: absolute;
+    top: 80px;
+    right: 0;
+    width: 100%;
+    background-color: darkslategrey;
+    z-index: 1;
+  }
+  .navbar-links li {
+    text-align: center;
+    padding: 1rem;
   }
 }
 </style>
